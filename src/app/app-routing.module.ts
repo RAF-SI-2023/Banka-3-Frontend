6bac759043a1ebf9b2a702cdd26bdddce0565448
mitrovic-l <mitrovic.luka@outlook.com>
2024-03-12 09:47:01 +0100

    izrada logina html i css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import { UserListComponent } from './user-list/user-list.component';
 
 import { RegisterComponent } from './register/register.component';
 import { RegisterCodeComponent } from './register-code/register-code.component';
-import { RegisterPasswordComponent} from './register-password/register-password.component';
+import { RegisterPasswordComponent } from './register-password/register-password.component';
+import { UserloginComponent } from './login/userlogin/userlogin.component';
+import { AdminLoginComponent } from './login/admin-login/admin-login.component';
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'register/code', component: RegisterCodeComponent },
   { path: 'register/password', component: RegisterPasswordComponent },
-  { path: 'user-list', component: UserListComponent}
+  { path: 'user-list', component: UserListComponent },
+  { path: 'user-login', component: UserloginComponent },
+  { path: 'admin-login', component: AdminLoginComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,17 +11,22 @@ import { RegisterCodeComponent } from './register-code/register-code.component';
 import { RegisterPasswordComponent } from './register-password/register-password.component';
 import { FormsModule } from '@angular/forms';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserloginComponent } from './login/userlogin/userlogin.component';
+import { AdminLoginComponent } from './login/admin-login/admin-login.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent
+    UserListComponent,
+    UserloginComponent,
+    AdminLoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatTableModule
+    MatTableModule,
     RegisterComponent,
     RegisterCodeComponent,
     RegisterPasswordComponent,
