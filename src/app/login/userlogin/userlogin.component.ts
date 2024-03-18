c5fa026c2301c52af63650751f8edcf15a7ab1bf
TisicPetar <ptisic5020rn@raf.rs>
2024-03-18 17:29:57 +0100

    Izmenjena trenutna login forma, dodate forme: provera mejla, postavljanje sifre i klasicno logovanje.

--- src/app/login/userlogin/userlogin.component.ts
+++ src/app/login/userlogin/userlogin.component.ts
@@ -8,20 +8,58 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./userlogin.component.css']
 })
 export class UserloginComponent {
+  codeActivate: boolean = false;
+  showCheckAddress: boolean = true;
+
+  checkEmailForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+  })
+
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', Validators.required)
   })
 
+  passwordForm = new FormGroup({
+    activationCode: new FormControl('', [Validators.required, Validators.pattern(/^\d{6}$/)]),
+    password: new FormControl('', Validators.required),
+    confirmPassword: new FormControl('', Validators.required)
+  })
+
+
+  //  Proverava da li postoji korisnik sa datim mejlom
+  checkAddr() {
+
+  this.showCheckAddress = false;
+  }
+
+
+  // Kreira usera sa datom adresom i vraca ga nazad na login
+  createUser() {
+
+  }
+
   submitLogin() {
-    console.log("SUbmit forme");
 
   }
 
   get email() {
     return this.loginForm.get('email');
   }
+  get emailCheck(){
+    return this.checkEmailForm.get('email')
+  }
+
   get password() {
     return this.loginForm.get('password');
+  }
+  get activationCode() {
+    return this.passwordForm.get('activationCode');
+  }
+  get newPassword() {
+    return this.passwordForm.get('password');
+  }
+  get confirmPassword() {
+    return this.passwordForm.get('confirmPassword')
   }
 }
