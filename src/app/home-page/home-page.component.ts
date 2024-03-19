f3e4d1fc907127e711b924441d1b959bb18b61c8
Ognjen Radović <70062458+ognjenradovic@users.noreply.github.com>
2024-03-19 19:09:03 +0100

    Zapoceto novo placanje i menjacnica

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+  ownerName: string = ''; // Variable for owner name
+  accountNumber: string= ''; // Variable for account number
+  availableBalance: string = ''; // Variable for available balance
+  type: string = ''; // Variable for account type
+  accountState: string = ''; // Variable for account state
+  reservedFunds: string = ''; // Variable for reserved funds
+  selectedCurrency1: string= "EUR";
+  selectedCurrency2: string= "USD";
+  currencies: string[] = ['USD', 'EUR', 'GBP', 'JPY', 'AUD'];
+  currency1: number = 0;
+  currency2: number = 0;
 
+   backToMain() {
+    const modalWrapper = document.querySelector('.myModalWrapper') as HTMLElement;
+    if (modalWrapper) {
+        modalWrapper.style.display = 'none';
+    } else {
+        console.error('Modal wrapper element not found.');
+    }
+}
+
+displayDetails(){
+  const modalWrapper = document.querySelector('.myModalWrapper') as HTMLElement;
+  if (modalWrapper) {
+      modalWrapper.style.display = 'flex';
+  } else {
+      console.error('Modal wrapper element not found.');
+  }
+}
+
+  // Function to refresh the page
+  newPayment() {
+    window.location.reload();
+  }
 }
