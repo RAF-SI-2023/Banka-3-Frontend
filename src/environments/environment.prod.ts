0cf7bd552e271c7c49006ebdeb9f7c91e650b5ac
DimitrijeGlibic <dimitrije.glibic1@gmail.com>
2024-05-12 22:59:56 +0200

    Add environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    userServiceUrl: 'banka-3-dev.si.raf.edu.rs/user-service',
+    bankServiceUrl: 'banka-3-dev.si.raf.edu.rs/bank-service',
+    exchangeServiceUrl: 'banka-3-dev.si.raf.edu.rs/exchange-service',
+    emailServiceUrl: 'banka-3-dev.si.raf.edu.rs/email-service'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    userServiceUrl: 'localhost:8080',
+    bankServiceUrl: 'localhost:8082',
+    exchangeServiceUrl: 'localhost:8083',
+    emailServiceUrl: 'localhost:8081'
+};
