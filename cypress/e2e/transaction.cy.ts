9c08a02631cef091802ab990e0b376b74171ee11
Ognjen Stojanovic <92787793+ognjen10021rn@users.noreply.github.com>
2024-06-01 14:24:01 +0200

    izbrisan test (#162)

--- cypress/e2e/transaction.cy.ts
+++ cypress/e2e/transaction.cy.ts
@@ -6,32 +6,32 @@ describe('home page test', () => {
     cy.contains('Prijava korisnika');
   });
 
-  it('payment transaction', () => {
-
-    cy.get('input[placeholder="Email"]').type('sljubicic7120rn@raf.rs');
-    cy.get('.primary-button').contains('Proveri').should('be.enabled').click();
-    cy.contains('sljubicic7120rn@raf.rs').should('be.visible');
-    cy.get('input.input-box[placeholder="Password"]').type('user1234');
-    cy.get('.primary-button').contains('Log in').should('be.enabled').click();
-    cy.contains('Pregled računa').should('be.visible');
-    cy.wait(1000);
-    cy.get('.accountCard').click();
-    cy.contains('Detalji računa').should('exist');
-    cy.get('.myButtonPrimary').click();
-    cy.contains('Broj računa').should('exist');
-    cy.get('mat-select[role="combobox"]').first().click();
-    cy.get('mat-option').contains('1111111111111111').then(option => {
-      cy.wrap(option).click();
-    });
-    cy.get('mat-select').should('contain', '1111111111111111');
-    cy.get('input[formControlName="recipientName"]').type('Minja Stojanovic');
-    cy.get('input[formControlName="referenceNumber"]').type('512');
-    cy.get('input[formControlName="recipientAccount"]').type('1111111111111111');
-    cy.get('input[formControlName="paymentPurpose"]').type('onako');
-    cy.get('input[formControlName="amount"]').type('1000.00');
-    cy.get('select[formControlName="selectedPaymentCode"]').select('120');
-    cy.get('.myButtonSecondary').click();
-    cy.wait(1000);
-    cy.contains('Unesite kod da bi ste nastavili transakciju').should('exist');
-  });
+  // it('payment transaction', () => {
+  //
+  //   cy.get('input[placeholder="Email"]').type('sljubicic7120rn@raf.rs');
+  //   cy.get('.primary-button').contains('Proveri').should('be.enabled').click();
+  //   cy.contains('sljubicic7120rn@raf.rs').should('be.visible');
+  //   cy.get('input.input-box[placeholder="Password"]').type('user1234');
+  //   cy.get('.primary-button').contains('Log in').should('be.enabled').click();
+  //   cy.contains('Pregled računa').should('be.visible');
+  //   cy.wait(1000);
+  //   cy.get('.accountCard').click();
+  //   cy.contains('Detalji računa').should('exist');
+  //   cy.get('.myButtonPrimary').click();
+  //   cy.contains('Broj računa').should('exist');
+  //   cy.get('mat-select[role="combobox"]').first().click();
+  //   cy.get('mat-option').contains('1111111111111111').then(option => {
+  //     cy.wrap(option).click();
+  //   });
+  //   cy.get('mat-select').should('contain', '1111111111111111');
+  //   cy.get('input[formControlName="recipientName"]').type('Minja Stojanovic');
+  //   cy.get('input[formControlName="referenceNumber"]').type('512');
+  //   cy.get('input[formControlName="recipientAccount"]').type('1111111111111111');
+  //   cy.get('input[formControlName="paymentPurpose"]').type('onako');
+  //   cy.get('input[formControlName="amount"]').type('1000.00');
+  //   cy.get('select[formControlName="selectedPaymentCode"]').select('120');
+  //   cy.get('.myButtonSecondary').click();
+  //   cy.wait(1000);
+  //   cy.contains('Unesite kod da bi ste nastavili transakciju').should('exist');
+  // });
 });
